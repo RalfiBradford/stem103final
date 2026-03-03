19514563ecbdca36abd68e376201223fe5bda92d
RalfiBradford <rwbradford@students.everettcc.edu>
2026-03-03 00:07:22 +0000

    work

---
+++ class.py
@@ -0,0 +1,19 @@
+import math
+# four most common types of errors in the stack trace
+
+# 1. syntax error
+# print(Hello World")
+
+# 2. type error
+a = 9
+b = "apple"
+
+# sum = a + b  int and str can't be added
+
+# 3. Name error
+car_value = 0
+car_speed = 100
+# print(car_values) make sure variable names are precise
+
+# 4. Value error
+# math.sqrt(-10)
